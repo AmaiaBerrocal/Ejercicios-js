8d1de3208314e6cc5ab1337c774b4e12a5edb17b
JJessa <jessicamosteiro13@gmail.com>
2023-02-13 13:55:01 +0100

    comentado la opción de seleccione idioma

--- ejer10022023/script.js
+++ ejer10022023/script.js
@@ -2,19 +2,23 @@ const menu = document.getElementById("menu");
 const parrafo = document.getElementById("result");
 
 const days = [
-    {Domingo, Lunes, Martes, Miércoles, Jueves, Viernes, Sábado},
-    {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday},
-    {Igandea, Astelehena, Asteartea, Asteazkena, Osteguna, Ostirala, Larunbata}
+    ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    ["Igandea", "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata"]
 ]
 
+let valor = "";
+
 menu.addEventListener('click', function(event) {
-    let valor = event.target.value;
+    valor = event.target.value;
+    // console.log(event.target);
+    // console.log(valor);
     mostrarFecha(valor);
 })
+
 
 function mostrarFecha(valor) {
     //crear una fecha, sacamos el dia en num 0=domingo
     let day = new Date().getDay();
-    console.log(day)
     parrafo.innerHTML = days[valor][day];
 }
